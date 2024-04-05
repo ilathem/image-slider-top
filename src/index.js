600aac8239095a595f0cf0e87ef2e78a49ab1775
ilathem <isaiahlathem@gmail.com>
2024-04-04 22:36:16 -0500

    Create wide image container

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./index.css";
+import cat from "./cat.jpg";
+import deer from "./deer.jpg";
+import forest from "./forest.jpg";
+import mtns from "./mtns.jpg";
+import waterfall from "./waterfall.jpg";
+
+let photos = [cat, deer, forest, mtns, waterfall];
+
+photos.forEach((photo) => {
+  const imgElement = document.createElement("img");
+  imgElement.src = photo;
+  imgElement.classList.add("image");
+  document.querySelector(".images").appendChild(imgElement);
+});
