e1aa8664130f46f7421344fbcfcac3a164aa660a
ilathem <isaiahlathem@gmail.com>
2024-04-09 18:36:44 -0500

    Images change on arrow key press

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import "./index.css";
-import cat from "./cat.jpg";
-import deer from "./deer.jpg";
-import forest from "./forest.jpg";
-import mtns from "./mtns.jpg";
-import waterfall from "./waterfall.jpg";
+import './index.css';
+import cat from './cat.jpg';
+import deer from './deer.jpg';
+import forest from './forest.jpg';
+import mtns from './mtns.jpg';
+import waterfall from './waterfall.jpg';
+let images = [cat, deer, forest, mtns, waterfall];
 
-let photos = [cat, deer, forest, mtns, waterfall];
+const addInitialImages = () => {
+  images.forEach((image) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image;
+    imgElement.classList.add('image');
+    document.querySelector('.images').appendChild(imgElement);
+  });
+};
 
-photos.forEach((photo) => {
-  const imgElement = document.createElement("img");
-  imgElement.src = photo;
-  imgElement.classList.add("image");
-  document.querySelector(".images").appendChild(imgElement);
+let currentImage = 0;
+
+const goRight = () => {
+  if (currentImage + 1 >= images.length) {
+    currentImage = 0;
+  } else currentImage = currentImage + 1;
+  // console.log(document.querySelector('.images').style)
+  document.querySelector('.images').style.left = `-${currentImage * 500}px`
+};
+
+const goLeft = () => {
+  if (currentImage - 1 < 0) {
+    currentImage = images.length - 1;
+  } else currentImage = currentImage - 1;
+  // console.log(document.querySelector('.images').style)
+  document.querySelector('.images').style.left = `-${currentImage * 500}px`
+};
+
+addInitialImages();
+
+document.body.addEventListener('keyup', (e) => {
+  if (e.key === 'ArrowRight') goRight();
+  if (e.key === 'ArrowLeft') goLeft();
 });
