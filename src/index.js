762a8d890207a22b3585dcd9bfbabe1941eeb59d
ilathem <isaiahlathem@gmail.com>
2024-04-09 22:49:09 -0500

    Add circles for navigation

--- src/index.js
+++ src/index.js
@@ -7,17 +7,25 @@ import waterfall from './waterfall.jpg';
 import arrow from './icons8-arrow-right-48.png';
 let images = [cat, deer, forest, mtns, waterfall];
 
+const circlesDiv = document.createElement('div');
+circlesDiv.classList.add('circlesDiv');
+
+const sliderDiv = document.createElement('div');
+sliderDiv.classList.add('sliderDiv');
+
+document.body.append(sliderDiv, circlesDiv);
+
 const leftArrow = document.createElement('img');
 leftArrow.src = arrow;
 leftArrow.classList.add('leftArrow');
-leftArrow.addEventListener('click', e => {
-  goLeft()
-})
-document.body.appendChild(leftArrow);
+leftArrow.addEventListener('click', (e) => {
+  goLeft();
+});
+sliderDiv.appendChild(leftArrow);
 
 const sliderContainer = document.createElement('div');
 sliderContainer.classList.add('slider-container');
-document.body.appendChild(sliderContainer);
+sliderDiv.appendChild(sliderContainer);
 
 const imagesDiv = document.createElement('div');
 imagesDiv.classList.add('images');
@@ -26,21 +34,38 @@ sliderContainer.appendChild(imagesDiv);
 const rightArrow = document.createElement('img');
 rightArrow.src = arrow;
 rightArrow.classList.add('rightArrow');
-rightArrow.addEventListener('click', e => {
-  goRight()
-})
-document.body.appendChild(rightArrow);
+rightArrow.addEventListener('click', (e) => {
+  goRight();
+});
+sliderDiv.appendChild(rightArrow);
 
 const addInitialImages = () => {
-  images.forEach((image) => {
+  images.forEach((image, index) => {
     const imgElement = document.createElement('img');
     imgElement.src = image;
     imgElement.classList.add('image');
     document.querySelector('.images').appendChild(imgElement);
+
+    const circle = document.createElement('circle');
+    circle.classList.add('circle');
+    if (index === 0) circle.classList.add('activeCircle');
+    circle.addEventListener('click', () => {
+      goToImage(index);
+      document.querySelectorAll('.circle').forEach((circle) => {
+        circle.classList.remove('activeCircle');
+      });
+      circle.classList.add('activeCircle');
+    });
+    circlesDiv.appendChild(circle);
   });
 };
 
 let currentImage = 0;
+
+const goToImage = (index) => {
+  currentImage = index;
+  document.querySelector('.images').style.left = `-${currentImage * 500}px`;
+};
 
 const goRight = () => {
   if (currentImage + 1 >= images.length) {
@@ -48,6 +73,10 @@ const goRight = () => {
   } else currentImage = currentImage + 1;
   // console.log(document.querySelector('.images').style)
   document.querySelector('.images').style.left = `-${currentImage * 500}px`;
+  document.querySelectorAll('.circle').forEach((circle, index) => {
+    if (currentImage === index) circle.classList.add('activeCircle');
+    else circle.classList.remove('activeCircle');
+  });
 };
 
 const goLeft = () => {
@@ -56,6 +85,10 @@ const goLeft = () => {
   } else currentImage = currentImage - 1;
   // console.log(document.querySelector('.images').style)
   document.querySelector('.images').style.left = `-${currentImage * 500}px`;
+  document.querySelectorAll('.circle').forEach((circle, index) => {
+    if (currentImage === index) circle.classList.add('activeCircle');
+    else circle.classList.remove('activeCircle');
+  });
 };
 
 addInitialImages();
